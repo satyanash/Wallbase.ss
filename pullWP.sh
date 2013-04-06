24cb1deb409e3513cf46d5b3fda1c929ab253ba2
satyanash <satyanash>
2013-04-06 20:23:32 +0530

    Corrected email address

--- pullWP.sh
+++ pullWP.sh
@@ -2,7 +2,7 @@
 ## This script will automatically pull a wallpaper list from Wallbase.cc
 ## choose a random one, and then apply it.
 ## Uses feh, an X11 background setter as the backend
-## Contact -- satyajeet@grumblegrumble.no-ip.org
+## Contact -- satyanash@rock.it.cx
 
 #######
 ##Purity flags, 1=true, 0=false
